640b19e3fc4bd195794011cf68545b98963e7f8a
SLemons1983 <s.lemons1983@gmail.com>
2026-07-05 02:02:55 +0000

    Add body map SVG infrastructure

---
+++ src/app/ePCR/clinical/components/body-map/svg/bodySvgBack.ts
@@ -0,0 +1,9 @@
+import type { ApolloBodySvgRegion } from './bodySvgTypes';
+
+/*
+Temporary placeholder paths.
+
+These will be replaced with the finished Apollo anatomical SVG.
+*/
+
+export const bodySvgBack: ApolloBodySvgRegion[] = [];
---
+++ src/app/ePCR/clinical/components/body-map/svg/bodySvgTypes.ts
@@ -0,0 +1,7 @@
+import type { ApolloBodyRegionKey } from '../bodyMapTypes';
+
+export type ApolloBodySvgRegion = {
+  id: ApolloBodyRegionKey;
+  path: string;
+  label: string;
+};
